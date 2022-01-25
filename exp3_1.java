dc252f64bcb73c419a0cd7d4d2de5fa9ffdd32d6
SahalBasheer <76899304+SahalBasheer@users.noreply.github.com>
2022-01-25 19:23:36 +0530

    Create exp3_1.java

---
+++ exp3_1.java
@@ -0,0 +1,46 @@
+//1. Program to create a student class with name, roll no and marks of five subjects. Write functions to read and display the details.
+import java.util.*;
+
+class exp3_1
+{
+ public static void main(String args[])
+ {
+  String n,g;
+  int c,r,t=0,i;
+  double p;
+  int m[]=new int[5];
+  Scanner obj = new Scanner(System.in);
+  System.out.print("Enter name of student: ");
+  n = in.nextLine();
+  System.out.print("Enter class of student: ");
+  c = in.nextInt();
+  System.out.print("Enter roll no of student: ");
+  r = in.nextInt();
+  for(i=0;i<5;i++)
+  {
+   System.out.print("Enter marks in 1st subject"+i+": ");
+   m[i] = in.nextInt();
+   t = t + m[i] ;
+  }
+  p = t / 500.0 * 100.0;
+  if (p >= 90)
+      g = "A";
+  else if (p >= 80)
+           g = "B";
+  else if (p >=70)
+           g = "C";
+  else if (p >= 60)
+           g = "D";
+  else if (p >= 50)
+           g = "E";
+  else if (p >= 40)
+           g = "P";
+  else
+      g = "F";
+            
+  System.out.println("Total Marks = " + t);
+  System.out.println("Percentage = " + p);
+  System.out.println("Grade = " + g);
+ }
+}
+ 
